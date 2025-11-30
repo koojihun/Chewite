10fb299dcf1391084f82b2e320f26b7e375bde17
koojihun <koo2788@naver.com>
2025-11-30 23:26:15 +0900

    Hilt 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt)
+    id("kotlin-kapt")
 }
 
 android {
@@ -39,6 +41,8 @@ android {
 }
 
 dependencies {
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
 
     // Credential Manager
     implementation(libs.androidx.credentials)
---
+++ app/src/main/java/com/chewite/app/ChewiteApplication.kt
@@ -0,0 +1,7 @@
+package com.chewite.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ChewiteApplication : Application()
